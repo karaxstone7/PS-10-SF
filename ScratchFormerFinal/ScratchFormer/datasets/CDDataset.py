da96f18db7241bf7d99bb07f038eeeea9dfbac84
karaxstone7 <karaxstone7@gmail.com>
2025-10-29 20:30:38 +0530

    Update CDDataset.py

--- ScratchFormerFinal/ScratchFormer/datasets/CDDataset.py
+++ ScratchFormerFinal/ScratchFormer/datasets/CDDataset.py
@@ -1,42 +1,35 @@
 import os
-import cv2
-import torch
-import numpy as np
 from torch.utils.data import Dataset
+from PIL import Image
+import numpy as np
+from torchvision import transforms
 
 class CDDataset(Dataset):
-    def __init__(self, root_dir, split='train', img_size=256, is_train=True):
+    def __init__(self, root_dir, split='train', img_size=256, is_train=True, label_transform=None):
         self.root_dir = root_dir
         self.split = split
         self.img_size = img_size
         self.is_train = is_train
+        self.label_transform = label_transform
 
-        # Read list of image names (without .png extension)
+        # Read image names from list file (without .png extension)
         list_path = os.path.join(root_dir, 'list', f'{split}.txt')
         with open(list_path, 'r') as f:
             self.img_list = [os.path.splitext(line.strip())[0] for line in f if line.strip()]
 
-        # LEVIR normalization stats
-        self.mean = np.array([0.40, 0.43, 0.45])
-        self.std = np.array([0.19, 0.18, 0.21])
+        # Standard transforms (resize + to tensor)
+        self.img_transform = transforms.Compose([
+            transforms.Resize((img_size, img_size)),
+            transforms.ToTensor()
+        ])
+
+        self.label_transform = label_transform or transforms.Compose([
+            transforms.Resize((img_size, img_size), interpolation=Image.NEAREST),
+            transforms.ToTensor()
+        ])
 
     def __len__(self):
         return len(self.img_list)
-
-    def preprocess_img(self, img_path):
-        img = cv2.imread(img_path, cv2.IMREAD_COLOR)  # BGR
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)    # to RGB
-        img = cv2.resize(img, (self.img_size, self.img_size), interpolation=cv2.INTER_LINEAR)
-        img = img.astype(np.float32) / 255.0
-        img = (img - self.mean) / self.std
-        img = torch.from_numpy(img).permute(2, 0, 1).float()
-        return img
-
-    def preprocess_label(self, label_path):
-        label = cv2.imread(label_path, cv2.IMREAD_GRAYSCALE)
-        label = cv2.resize(label, (self.img_size, self.img_size), interpolation=cv2.INTER_NEAREST)
-        label = torch.from_numpy(label).long().unsqueeze(0)  # [1, H, W]
-        return label
 
     def __getitem__(self, idx):
         name = self.img_list[idx]
@@ -45,8 +38,12 @@ class CDDataset(Dataset):
         img_B_path = os.path.join(self.root_dir, 'B', f'{name}.png')
         label_path = os.path.join(self.root_dir, 'label', f'{name}.png')
 
-        A = self.preprocess_img(img_A_path)
-        B = self.preprocess_img(img_B_path)
-        L = self.preprocess_label(label_path)
+        img_A = Image.open(img_A_path).convert('RGB')
+        img_B = Image.open(img_B_path).convert('RGB')
+        label = Image.open(label_path)
+
+        A = self.img_transform(img_A)
+        B = self.img_transform(img_B)
+        L = self.label_transform(label)
 
         return {'A': A, 'B': B, 'L': L}
